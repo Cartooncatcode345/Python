05040ba7bec6edaedb3e3bf51cce7714ad2f95a8
Cartooncatcode345 <nameer.mehran.ayaat@gmail.com>
2025-12-19 12:39:22 +0600

    Conditional Statements and Lists

---
+++ list.py
@@ -0,0 +1,32 @@
+lst = ['Apple' , 'Guava' , 'Mango' , 'Banana' , 'Kiwi']
+
+print("Length of list:", len(lst))
+print("First Element:", lst[0])
+print("Last Element:", lst[-1])
+
+lst.append('papaya')
+print("updated list:", lst)
+
+lst.remove('Guava')
+print("updated list:", lst)
+
+lst.sort()
+print("Sorted list:", lst)
+
+lst.pop(1)
+print("Updated list :", lst)
+
+lst.reverse()
+print("Reversed list :", lst)
+
+print("Multiplication on List :", lst *2 )
+      
+lst = lst[:4]
+print("Sliced list :", lst)
+
+lst.clear()
+print("Updated list :", lst)
+
+
+
+
---
+++ positive-and-negativenumbers.py
@@ -0,0 +1,12 @@
+number = int(input("Enter Number to Check"))
+print("Number to be checked :", number)
+
+if number>0:
+    print("this is a positive number")
+
+elif number<0:
+    print("this is a negative number")
+
+else:
+    print("this number is neutral")
+   
