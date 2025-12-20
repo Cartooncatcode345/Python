7bc71e497eb8824265b0a9e9a72f9607939578b5
Cartooncatcode345 <nameer.mehran.ayaat@gmail.com>
2025-12-20 12:52:13 +0600

    natural number

---
+++ naturalnumber.py
@@ -0,0 +1,9 @@
+number = int(input("Enter Number to Check"))
+print("Number to be checked :", number)
+
+if number>0:
+    print("this number is natural ")
+
+
+else:
+    print("this number is not natural")
