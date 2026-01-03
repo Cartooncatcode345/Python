e9f56c68abd0ad1180e9bb8f899d57c822104652
Cartooncatcode345 <nameer.mehran.ayaat@gmail.com>
2026-01-03 13:24:41 +0600

    random fake profile generator

--- tuples.py
+++ tuples.py
@@ -4,12 +4,13 @@ my_tuple3=("Nameer","Coding","football")
 print(my_tuple3)
 #len
 print(len(my_tuple3))
-print(my_tuple3("Nameer"))
-print(my_tuple3("Coding"))
-print(my_tuple3("football"))
+print(my_tuple3["Nameer"])
+print(my_tuple3["Coding"])
+print(my_tuple3["football"])
 
 print(my_tuple1)
 print(my_tuple2[3][5])
+
 
 for i in  my_tuple2:
     print(i)
